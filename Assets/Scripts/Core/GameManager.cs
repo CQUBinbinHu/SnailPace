0676b147b7cc2dfc0b5d0dac25c6df805c3748ab
ben <cquhbb@163.com>
2022-11-08 20:57:01 +0800

    debug trigger

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using MoreMountains.Tools;
+using UnityEngine;
+
+namespace Core
+{
+    public enum CoreGameEventTypes
+    {
+        Start,
+        GameOver,
+        Pause,
+        EncounterBattle
+    }
+
+    public struct CoreGameEvent
+    {
+        public CoreGameEventTypes EventType;
+
+        public CoreGameEvent(CoreGameEventTypes eventType)
+        {
+            EventType = eventType;
+        }
+
+        static CoreGameEvent e;
+
+        public static void Trigger(CoreGameEventTypes eventType)
+        {
+            e.EventType = eventType;
+            MMEventManager.TriggerEvent(e);
+        }
+    }
+
+    public class GameManager :
+        MMPersistentSingleton<GameManager>,
+        MMEventListener<CoreGameEvent>
+    {
+        public void OnMMEvent(CoreGameEvent eventType)
+        {
+            switch (eventType.EventType)
+            {
+                case CoreGameEventTypes.Start:
+                    break;
+                case CoreGameEventTypes.GameOver:
+                    break;
+                case CoreGameEventTypes.Pause:
+                    break;
+                case CoreGameEventTypes.EncounterBattle:
+                    Debug.Log("Debug EncounterBattle");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// OnDisable, we start listening to events.
+        /// </summary>
+        protected virtual void OnEnable()
+        {
+            this.MMEventStartListening<CoreGameEvent>();
+        }
+
+        /// <summary>
+        /// OnDisable, we stop listening to events.
+        /// </summary>
+        protected virtual void OnDisable()
+        {
+            this.MMEventStopListening<CoreGameEvent>();
+        }
+    }
+}
---
+++ Assets/Scripts/HeroTriggerBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Core;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class HeroTriggerBox : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            CoreGameEvent.Trigger(CoreGameEventTypes.EncounterBattle);
+        }
+    }
+}
